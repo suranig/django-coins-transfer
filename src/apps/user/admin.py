a29e7411a8ad568e1ca51e6ef573ad63030b8467
Tumiwisista <suranig7@gmail.com>
2019-10-28 23:03:59 +0100

    some changes

--- src/apps/user/admin.py
+++ src/apps/user/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ["username", "email", "balance"]
+
+
+admin.site.register(User, CustomUserAdmin)
